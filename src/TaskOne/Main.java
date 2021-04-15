e15714e7a0ccd2ac3ebb869ffc4f858822667fff
DonVitalko <DonVitalko@mail.ru>
2021-04-15 12:48:10 +0300

    Lesson one level three

---
+++ src/TaskOne/Main.java
@@ -0,0 +1,33 @@
+package TaskOne;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] arr = {"asd", "qwe", "zxc", "fgh","vbn"};
+
+        //вторая задача
+        List<String> list = arrayToList(arr);
+        System.out.println(list);
+
+        //первая задача
+        System.out.println(change(arr,1,3));
+
+    }
+
+    public static <T> List change(T[] arr, int indexOne, int indexTwo){
+        T buffer = arr[indexOne];
+        arr[indexOne] = arr[indexTwo];
+        arr[indexTwo] = buffer;
+        return Arrays.asList(arr);
+    }
+
+    public static <T> ArrayList arrayToList(T[] arr){
+        return new ArrayList<>(Arrays.asList(arr));
+    }
+
+
+}
---
+++ src/TaskTwo/Fruit.java
@@ -0,0 +1,4 @@
+package TaskTwo;
+
+public class Fruit {
+}
